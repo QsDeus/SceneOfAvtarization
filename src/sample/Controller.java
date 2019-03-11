11ff5117eec0ca9263aeb74983331146e4ba37d8
QsDeus <2048150@gmail.com>
2019-03-11 16:44:31 +0400

    add sql

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,16 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -29,8 +34,23 @@ public class Controller {
 
     @FXML
     void initialize() {
-        authSignButton.setOnAction(event -> {
-            System.out.println("Вы нажали на кнопку Войти");
+        loginSignButton.setOnAction(event -> {
+            loginSignButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
         });
 
 
